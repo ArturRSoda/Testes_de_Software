7d1c922ac84bcb640ae99121912afaf83cca40e1
ArturRSoda <artursodar@gmail.com>
2026-05-12 10:26:46 -0300

    test insercao funcionario fail

--- test_empresa.py
+++ test_empresa.py
@@ -1,8 +1,16 @@
 import unittest
 from empresa import Empresa
+from funcionario import Funcionario
 
 class ClassTesteEmpresa(unittest.TestCase):
     def test_criaEmpresa(self):
         empresa = Empresa("Elton-Lmtd")
         self.assertEqual(empresa.nome, "Elton-Lmtd")
+
+    def test_incluirFuncionario(self):
+        empresa = Empresa("Elton-Lmtd")
+        funcionario = Funcionario("Felipe")
+        empresa.inserir_funcionario(funcionario)
+        self.asserEqual(empresa.funcionario[0].nome, "Felipe")
+
 
