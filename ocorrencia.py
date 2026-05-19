84351dc7d33b8a31bb41ac985fecb44d7ab972f5
ArturRSoda <artursodar@gmail.com>
2026-05-19 10:45:25 -0300

    test definir prioridade ok

--- ocorrencia.py
+++ ocorrencia.py
@@ -4,9 +4,16 @@ class Ocorrencia:
         self.resumo = resumo
         self.estado = "aberto"
         self.tipo = "NA"
+        self.prioridade = "NA"
 
     def definir_tipo(self, tipo):
         if tipo not in ("bug", "tarefa", "refatoracao", "NA"):
             raise ValueError("Tipo de ocorrencia invalido")
 
         self.tipo = tipo
+
+    def definir_prioridade(self, prioridade):
+        if prioridade not in ("baixa", "media", "alta", "NA"):
+            raise ValueError("Prioridade de ocorrencia invalido")
+
+        self.prioridade = prioridade 
