7c3e8f02228b2148f022c0cc38c1d1964f5bae9d
ArturRSoda <artursodar@gmail.com>
2026-05-19 10:40:08 -0300

    test difinir tipo ocorrencia ok

--- ocorrencia.py
+++ ocorrencia.py
@@ -3,3 +3,10 @@ class Ocorrencia:
         self.nome = nome
         self.resumo = resumo
         self.estado = "aberto"
+        self.tipo = "NA"
+
+    def definir_tipo(self, tipo):
+        if tipo not in ("bug", "tarefa", "refatoracao", "NA"):
+            raise ValueError("Tipo de ocorrencia invalido")
+
+        self.tipo = tipo
